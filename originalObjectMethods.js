d2a090b17c7efea927e7f92046ef83ac30e50e85
Nerevand <kruchkovskiy2@gmail.com>
2019-08-08 09:59:53 +0300

    added original functions with my methods

---
+++ originalObjectMethods.js
@@ -0,0 +1,17 @@
+//deep copy of object
+//for example Object.deepCopy(ourObj);
+Object.prototype.deepCopy = function (obj) {
+    if (typeof obj != "object") {
+        return obj;
+    }
+    
+    var copy = obj.constructor();
+    for (let key in obj) {
+        if (typeof obj[key] == "object") {
+            copy[key] = deepCopy(obj[key]);
+        } else {
+            copy[key] = obj[key];
+        }
+    }
+    return copy;
+};
